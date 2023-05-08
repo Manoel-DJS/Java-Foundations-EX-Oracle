d6f83455b9f2995eae5025c4041e6bdda9e25e4a
Manoel-DJS <vinesouza9x8@gmail.com>
2023-05-07 21:23:31 -0300

    Tarefa 1 curso Oracle

---
+++ Problema 1 Arte ASCII Simples/Arte_ASCII/src/App.java
@@ -0,0 +1,28 @@
+/** 
+
+Problema 1: Arte ASCII Simples
+Visão Geral
+O uso de texto para criar uma imagem é um processo conhecido 
+como arte ASCII. Na seção 2, criamos um gato usando arte ASCII. 
+Neste exercício, você usará instruções de impressão para 
+recriar a imagem acima.
+
+Tarefa
+Use oito instruções de impressão para recriar a carinha feliz acima. 
+Sua arte baseia-se em um único caractere, além do espaço, 
+como X ou #.
+
+**/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("   ####   ");
+        System.out.println(" #      # ");
+        System.out.println("# #    # #");
+        System.out.println("#        #");
+        System.out.println("#  #  #  #");
+        System.out.println("#   ##   #");
+        System.out.println(" #      # ");
+        System.out.println("   ####   ");
+    }
+}
---
+++ Problema 1 Arte ASCII Simples/Arte_ASCII/src/Tarefa02.java
@@ -0,0 +1,33 @@
+/** 
+
+Tarefa
+Use instruções de impressão para criar sua própria arte ASCII original bonita. Use comentários para descrever o que ela está
+retratando.
+Não há problema em sua arte basear-se apenas em um caractere, além do espaço, como X ou #.
+Mas considere o uso de alguns caracteres diferentes no seu design, como aconteceu no exemplo do
+gato da classe.
+Sua arte também deve:
+ Usar pelo menos oito instruções de impressão
+ Ter pelo menos oito caracteres de largura
+ Usar pelo menos 20 caracteres que não sejam espaço
+
+**/
+
+public class Tarefa02 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Robo
+        System.out.println("      _____     ");
+        System.out.println("     | === |    ");
+        System.out.println("     |_____|    ");
+        System.out.println("   //######||   ");
+        System.out.println("  // ###### ||  ");
+        System.out.println(" //  ###### ||  ");
+        System.out.println("     ^^^^^^     ");
+        System.out.println("    | # | # |   ");
+        System.out.println("    | # | # |   ");
+        System.out.println("    [__ | __]   ");        
+    }
+}
