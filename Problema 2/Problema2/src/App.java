77844f3d244fb8de28637e17d4f0024991a9b5de
Manoel-DJS <vinesouza9x8@gmail.com>
2023-05-13 20:26:33 -0300

    Problema 2 do Java Foundations

---
+++ Problema 2/Problema2/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Se deixar o System com s minúsculo da erro
+
+        // Quest01
+        /** 
+        Edite o código de modo que ele imprima uma mensagem diferente de “Hello World!”
+        **/
+        System.out.println("@ Olá Senhores! @");
+        
+        // Quest02
+        /** 
+        A sintaxe Java faz distinção entre letras maiúsculas e minúsculas?
+        Sim. O java Sim, a linguagem Java diferencia as maiúsculas e minúsculas
+        **/
+        
+         // Quest03
+        /** 
+        O ponto e vírgula é importante para a sintaxe Java?
+        Porque ele é usado para indicar o fim de uma instrução.
+        **/
+    }
+}
