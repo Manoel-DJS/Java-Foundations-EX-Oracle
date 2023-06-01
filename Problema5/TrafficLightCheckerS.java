cd1d567a9ce067c78510cb890818ff91cd736296
Manoel-DJS <vinesouza9x8@gmail.com>
2023-06-01 11:07:41 -0300

    Exercícios - Seção 5 Problema 5-3
    Determinando a próxima cor de um sinal de trânsito usando switch
    Mesmo do anterior, só coloquei o switch

---
+++ Problema5/TrafficLightCheckerS.java
@@ -0,0 +1,42 @@
+package Problema5;
+import java.util.Scanner;
+
+public class TrafficLightCheckerS { // Usando a instrução switch.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADO
+
+        System.out.println("Digite a cor atual: ");
+        int currentColor = scanner.nextInt();
+
+        scanner.close();
+        
+        String TrafficLight ="";
+
+        // PROCESSAMENTO
+
+        switch(currentColor){
+            case 1: 
+                TrafficLight = "Next Traffic Light is Green";
+                break;
+
+            case 2:
+                TrafficLight = "Next Traffic Light is Yellow";
+                break;
+                
+            case 3:
+                TrafficLight = "Next Traffic Light is Red";
+                break;
+
+            default: 
+                TrafficLight = "Invalid color // Digite um número válido!";
+                break;
+        }
+
+        // SAÍDA
+
+        System.out.println(TrafficLight);
+        
+    }
+}
